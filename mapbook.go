051100619024e60b49e6af08e0fe7361f6b79496
Bo-Hao <bohaoperng@gmail.com>
2022-02-27 22:07:56 +0800

    20220227 simplified

--- mapbook.go
+++ mapbook.go
@@ -1,6 +1,8 @@
 package mapbook
 
 import (
+	"fmt"
+	"log"
 	"sort"
 
 	"github.com/shopspring/decimal"
@@ -11,8 +13,7 @@ import (
 // Ask
 func NewAskBook(IsEvent bool) *AskBook {
 	return &AskBook{
-		OrderKey: []string{},
-		Book:     map[string]string{},
+		Book: map[string]string{},
 
 		isEvent: IsEvent,
 		event:   [][]string{},
@@ -20,16 +21,8 @@ func NewAskBook(IsEvent bool) *AskBook {
 }
 
 func (book *AskBook) Snapshot(snapshot [][]string) {
-	orderkey := make([]string, len(snapshot))
 	book.Lock()
 	defer book.Unlock()
-
-	// from small to big
-	sort.Slice(snapshot, func(i, j int) bool {
-		oi, _ := decimal.NewFromString(snapshot[i][0])
-		oj, _ := decimal.NewFromString(snapshot[j][0])
-		return oi.LessThan(oj)
-	})
 
 	for i := range snapshot {
 		order := snapshot[i]
@@ -37,159 +30,62 @@ func (book *AskBook) Snapshot(snapshot [][]string) {
 		volume := order[1]
 
 		book.Book[price] = volume
-		orderkey[i] = price
 	}
-
-	book.OrderKey = orderkey
 }
 
 func (book *AskBook) Update(update [][]string) {
-
-	sort.Slice(update, func(i, j int) bool {
-		oi, _ := decimal.NewFromString(update[i][0])
-		oj, _ := decimal.NewFromString(update[j][0])
-		return oi.LessThan(oj)
-	})
-
 	book.Lock()
 	defer book.Unlock()
 
-	idx := 0
-	delKeyIdx := 0
 	for i := range update {
 		order := update[i]
 		price := order[0]
 		volume := order[1]
 
-		alreadyIn := false
 		switch {
 		case volume == "0":
 			delete(book.Book, price)
-			alreadyIn = true
-
-			for delKeyIdx < len(book.OrderKey) {
-				if book.OrderKey[delKeyIdx] == price {
-					book.OrderKey = append(book.OrderKey[:delKeyIdx], book.OrderKey[delKeyIdx+1:]...)
-					break
-				}
-				delKeyIdx++
-			}
 
 		case volume != "0":
 			if _, ok := book.Book[price]; ok {
-				alreadyIn = true
 				book.Book[price] = volume
 			} else {
 				book.Book[price] = volume
 			}
 		}
-
-		if !alreadyIn {
-			decPrice, _ := decimal.NewFromString(price)
-			for j := idx; j < len(book.OrderKey); j++ {
-				decKey, _ := decimal.NewFromString(book.OrderKey[j])
-				if decPrice.LessThan(decKey) {
-					book.OrderKey = append(append(book.OrderKey[:j], price), book.OrderKey[j:]...)
-					idx = j
-					break
-				}
-			}
-		}
-
-	}
-}
-
-func (book *AskBook) GetTop(n int) ([][]string, bool) {
-	asks := make([][]string, 0, n)
-	vanishKey := []string{}
-
-	book.RLock()
-	orderkey := book.OrderKey
-	book.RUnlock()
-	for i := range orderkey {
-		price := orderkey[i]
-		if _, ok := book.Book[price]; ok {
-			volume := book.Book[price]
-			if volume != "0" {
-				asks = append(asks, []string{price, volume})
-			}
-		} else {
-			vanishKey = append(vanishKey, price)
-		}
-
-		if len(asks) >= n {
-			break
-		}
-	}
-
-	k := 0
-	i := 0
-
-	book.Lock()
-	for i < len(book.OrderKey) {
-		for j := k; j < len(vanishKey); j++ {
-			if book.OrderKey[i] == vanishKey[j] {
-				book.OrderKey = append(book.OrderKey[:i], book.OrderKey[i+1:]...)
-				i--
-				k++
-			}
-		}
-		if k == len(vanishKey)-1 {
-			break
-		}
-		i++
-	}
-	book.Unlock()
-
-	if len(asks) >= n {
-		return asks, true
-	} else {
-		return asks, false
 	}
 }
 
 func (book *AskBook) GetAll() ([][]string, bool) {
-	var asks [][]string
-	vanishKey := []string{}
-
 	book.RLock()
-	orderkey := book.OrderKey
+	Book := book.Book
 	book.RUnlock()
 
-	for i := range orderkey {
-		price := orderkey[i]
-		if _, ok := book.Book[price]; ok {
-			volume := book.Book[price]
-			if volume != "0" {
-				asks = append(asks, []string{price, volume})
-			}
-		} else {
-			vanishKey = append(vanishKey, price)
+	prices := make([]decimal.Decimal, 0, len(book.Book))
+	for k := range Book {
+		p, err := decimal.NewFromString(k)
+		if err != nil {
+			log.Println(err)
 		}
+		prices = append(prices, p)
 	}
 
-	k := 0
-	i := 0
+	// from small to big
+	sort.Slice(prices, func(i, j int) bool {
+		return prices[i].LessThan(prices[j])
+	})
 
-	book.Lock()
-	for i < len(book.OrderKey) {
-		for j := k; j < len(vanishKey); j++ {
-			if book.OrderKey[i] == vanishKey[j] {
-				book.OrderKey = append(book.OrderKey[:i], book.OrderKey[i+1:]...)
-				i--
-				k++
-			}
-		}
-		if k == len(vanishKey)-1 {
-			break
-		}
-		i++
+	asks := make([][]string, 0, len(prices))
+	for i := range prices {
+		p := prices[i].String()
+		v := Book[p]
+		asks = append(asks, []string{p, v})
 	}
-	book.Unlock()
 
 	if len(asks) != 0 {
 		return asks, true
 	} else {
+		fmt.Println(book)
 		return asks, false
 	}
 }
@@ -197,8 +93,7 @@ func (book *AskBook) GetAll() ([][]string, bool) {
 // bid
 func NewBidBook(IsEvent bool) *BidBook {
 	return &BidBook{
-		OrderKey: []string{},
-		Book:     map[string]string{},
+		Book: map[string]string{},
 
 		isEvent: IsEvent,
 		event:   [][]string{},
@@ -206,16 +101,8 @@ func NewBidBook(IsEvent bool) *BidBook {
 }
 
 func (book *BidBook) Snapshot(snapshot [][]string) {
-	orderkey := make([]string, len(snapshot))
 	book.Lock()
 	defer book.Unlock()
-
-	// from small to big
-	sort.Slice(snapshot, func(i, j int) bool {
-		oi, _ := decimal.NewFromString(snapshot[i][0])
-		oj, _ := decimal.NewFromString(snapshot[j][0])
-		return oi.GreaterThan(oj)
-	})
 
 	for i := range snapshot {
 		order := snapshot[i]
@@ -223,159 +110,62 @@ func (book *BidBook) Snapshot(snapshot [][]string) {
 		volume := order[1]
 
 		book.Book[price] = volume
-		orderkey[i] = price
 	}
-
-	book.OrderKey = orderkey
 }
 
 func (book *BidBook) Update(update [][]string) {
-
-	sort.Slice(update, func(i, j int) bool {
-		oi, _ := decimal.NewFromString(update[i][0])
-		oj, _ := decimal.NewFromString(update[j][0])
-		return oi.GreaterThan(oj)
-	})
-
 	book.Lock()
 	defer book.Unlock()
 
-	idx := 0
-	delKeyIdx := 0
 	for i := range update {
 		order := update[i]
 		price := order[0]
 		volume := order[1]
 
-		alreadyIn := false
 		switch {
 		case volume == "0":
 			delete(book.Book, price)
-			alreadyIn = true
-
-			for delKeyIdx < len(book.OrderKey) {
-				if book.OrderKey[delKeyIdx] == price {
-					book.OrderKey = append(book.OrderKey[:delKeyIdx], book.OrderKey[delKeyIdx+1:]...)
-					break
-				}
-				delKeyIdx++
-			}
 
 		case volume != "0":
 			if _, ok := book.Book[price]; ok {
-				alreadyIn = true
 				book.Book[price] = volume
 			} else {
 				book.Book[price] = volume
 			}
 		}
-
-		if !alreadyIn {
-			decPrice, _ := decimal.NewFromString(price)
-			for j := idx; j < len(book.OrderKey); j++ {
-				decKey, _ := decimal.NewFromString(book.OrderKey[j])
-				if decPrice.GreaterThan(decKey) {
-					book.OrderKey = append(append(book.OrderKey[:j], price), book.OrderKey[j:]...)
-					idx = j
-					break
-				}
-			}
-		}
-
-	}
-}
-
-func (book *BidBook) GetTop(n int) ([][]string, bool) {
-	bids := make([][]string, 0, n)
-	vanishKey := []string{}
-
-	book.RLock()
-	orderkey := book.OrderKey
-	book.RUnlock()
-	for i := range orderkey {
-		price := orderkey[i]
-		if _, ok := book.Book[price]; ok {
-			volume := book.Book[price]
-			if volume != "0" {
-				bids = append(bids, []string{price, volume})
-			}
-		} else {
-			vanishKey = append(vanishKey, price)
-		}
-
-		if len(bids) >= n {
-			break
-		}
-	}
-
-	k := 0
-	i := 0
-
-	book.Lock()
-	for i < len(book.OrderKey) {
-		for j := k; j < len(vanishKey); j++ {
-			if book.OrderKey[i] == vanishKey[j] {
-				book.OrderKey = append(book.OrderKey[:i], book.OrderKey[i+1:]...)
-				i--
-				k++
-			}
-		}
-		if k == len(vanishKey)-1 {
-			break
-		}
-		i++
-	}
-	book.Unlock()
-
-	if len(bids) >= n {
-		return bids, true
-	} else {
-		return bids, false
 	}
 }
 
 func (book *BidBook) GetAll() ([][]string, bool) {
-	var bids [][]string
-	vanishKey := []string{}
-
 	book.RLock()
-	orderkey := book.OrderKey
+	Book := book.Book
 	book.RUnlock()
 
-	for i := range orderkey {
-		price := orderkey[i]
-		if _, ok := book.Book[price]; ok {
-			volume := book.Book[price]
-			if volume != "0" {
-				bids = append(bids, []string{price, volume})
-			}
-		} else {
-			vanishKey = append(vanishKey, price)
+	prices := make([]decimal.Decimal, 0, len(book.Book))
+	for k := range Book {
+		p, err := decimal.NewFromString(k)
+		if err != nil {
+			log.Println(err)
 		}
+		prices = append(prices, p)
 	}
 
-	k := 0
-	i := 0
+	// from small to big
+	sort.Slice(prices, func(i, j int) bool {
+		return prices[i].GreaterThan(prices[j])
+	})
 
-	book.Lock()
-	for i < len(book.OrderKey) {
-		for j := k; j < len(vanishKey); j++ {
-			if book.OrderKey[i] == vanishKey[j] {
-				book.OrderKey = append(book.OrderKey[:i], book.OrderKey[i+1:]...)
-				i--
-				k++
-			}
-		}
-		if k == len(vanishKey)-1 {
-			break
-		}
-		i++
+	bids := make([][]string, 0, len(prices))
+	for i := range prices {
+		p := prices[i].String()
+		v := Book[p]
+		bids = append(bids, []string{p, v})
 	}
-	book.Unlock()
 
 	if len(bids) != 0 {
 		return bids, true
 	} else {
+		fmt.Println(book)
 		return bids, false
 	}
 }
